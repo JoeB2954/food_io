d7aa879ae4205eead08765dd9122a5902a486d5c
ConnorJ88 <123974095+ConnorJ88@users.noreply.github.com>
2024-04-30 11:28:04 +0000

    Functionality Added to main to allow users to pick an imae from photos or use camera

--- lib/main.dart
+++ lib/main.dart
@@ -1,126 +1,65 @@
 import 'package:flutter/material.dart';
-import 'dart.io';
+
+import 'dart:io';
 import 'package:image_picker/image_picker.dart';
-void main() {
-  runApp(const MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() => runApp(const MealIO());
 
-  // This widget is the root of your application.
+class MealIO extends StatefulWidget{
+  const MealIO({super.key});
+
+  
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Meal.IO',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Meal.IO Home Page'),
-    );
+  State<StatefulWidget> createState(){
+  
+    return MealIOState();
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MealIOState extends State<MealIO> {
+  
+  File ? _image;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text ('Meal.IO'),
+        ),
+        body: Center(
+           child: Column(
+            children: <Widget>[
+              IconButton( icon: const Icon(Icons.insert_drive_file),
+              onPressed: () {
+                _pickImageFromGallery();
+              },
+              ),
+              const SizedBox(height: 10.0,),
+              IconButton( icon: const Icon(Icons.camera_alt),
+              onPressed: () {
+                _pickImageFromCamera();
+              },
+              ),
+              
+            ],
+           ),
+          ),
+        ),
+    );
+  }
+  Future _pickImageFromGallery() async {
+    final returnedImage = await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (returnedImage == null) return;
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _image = File(returnedImage.path);
     });
   }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            IconButton: ( 
-                icon: Icon(Icons.insert_drive_file),
-                onPressed: (){},
-                ), // This trailing comma makes auto-formatting nicer for build methods.
-            SizedBox(height: 10.0.),
-            IconButton: ( 
-                icon: Icon(Icons.camera_alt),
-                onPressed: (){},
-                ),
-
-          ],
-        ),
-      ),
-      
-    );
+  Future _pickImageFromCamera() async {
+    final returnedImage = await ImagePicker().pickImage(source: ImageSource.camera);
+    if (returnedImage == null) return;
+    setState(() {
+      _image = File(returnedImage.path);
+    });
   }
 }
