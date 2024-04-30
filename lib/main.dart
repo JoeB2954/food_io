46eef10bcee27d0520837cb3b10a401dcd7486a1
ConnorJ88 <123974095+ConnorJ88@users.noreply.github.com>
2024-04-30 13:49:30 +0000

    added models, views services folders, moved homepage into views folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,16 @@
 import 'package:flutter/material.dart';
 
-import 'dart:io';
-import 'package:image_picker/image_picker.dart';
 
 void main() => runApp(const MealIO());
 
-class MealIO extends StatefulWidget{
-  const MealIO({super.key});
-
-  
-  @override
-  State<StatefulWidget> createState(){
-  
-    return MealIOState();
-  }
-}
-
-class MealIOState extends State<MealIO> {
-  
-  File ? _image;
-
+class MealIO extends StatelessWidget {
+  const MealIO({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text ('Meal.IO'),
-        ),
-        body: Center(
-           child: Column(
-            children: <Widget>[
-              IconButton( icon: const Icon(Icons.insert_drive_file),
-              onPressed: () {
-                _pickImageFromGallery();
-              },
-              ),
-              const SizedBox(height: 10.0,),
-              IconButton( icon: const Icon(Icons.camera_alt),
-              onPressed: () {
-                _pickImageFromCamera();
-              },
-              ),
-              
-            ],
-           ),
-          ),
-        ),
+    title: 'Meal.IO',
+    theme: ThemeData(primarySwatch: Colors.blue,),
+    home: const MealIO(),
     );
-  }
-  Future _pickImageFromGallery() async {
-    final returnedImage = await ImagePicker().pickImage(source: ImageSource.gallery);
-    if (returnedImage == null) return;
-    setState(() {
-      _image = File(returnedImage.path);
-    });
-  }
-  Future _pickImageFromCamera() async {
-    final returnedImage = await ImagePicker().pickImage(source: ImageSource.camera);
-    if (returnedImage == null) return;
-    setState(() {
-      _image = File(returnedImage.path);
-    });
   }
 }
