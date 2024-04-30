1f92d1bfb9320dbba8582e18f113eab43e16aece
ConnorJ88 <123974095+ConnorJ88@users.noreply.github.com>
2024-04-30 19:24:07 +0000

    updated main import

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:food_io/views/home_page.dart';
-=======
 import 'package:googleapis_auth/auth_io.dart';
 import 'dart:io';
+import 'package:food_io/views/home_page.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:http/http.dart' as http;
 import 'package:googleapis/vision/v1.dart';
->>>>>>> e1924a430aa60c780f4c1b3106e082bf38c09533
 
 void main() => runApp(MealIO());
 
