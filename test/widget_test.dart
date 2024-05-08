8fcdcfc8ddfec5d901f68f694688c26517f5e68f
ConnorJ88 <123974095+ConnorJ88@users.noreply.github.com>
2024-05-08 22:16:35 +0000

    Added tests to the test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,29 +1,26 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:food_io/main.dart';
 
-//import 'package:flutter/material.dart';
-//import 'package:flutter_test/flutter_test.dart';
+void main() {
+  testWidgets('MyHomePage widget test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MaterialApp(
+      home: MyHomePage(),
+    ));
 
-///
-//void main() {
- // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
- //   // Build our app and trigger a frame.
- //   await tester.pumpWidget(const MyApp());
+    // Verify that the title text is displayed.
+    expect(find.text('Meal.IO'), findsOneWidget);
 
-  //  // Verify that our counter starts at 0.
- //   expect(find.text('0'), findsOneWidget);
- //   expect(find.text('1'), findsNothing);
+    // Verify that the two ElevatedButtons are displayed.
+    expect(find.byIcon(Icons.perm_media_rounded), findsOneWidget);
+    expect(find.byIcon(Icons.camera_alt), findsOneWidget);
 
-  //  // Tap the '+' icon and trigger a frame.
- //   await tester.tap(find.byIcon(Icons.add));
-  //  await tester.pump();
+    // Tap on the ElevatedButton to pick image from gallery.
+    await tester.tap(find.byIcon(Icons.perm_media_rounded));
+    await tester.pump();
 
-    // Verify that our counter has incremented.
-  //  expect(find.text('0'), findsNothing);
-  //  expect(find.text('1'), findsOneWidget);
- // });
-//}
+    // Verify that the Navigator pushes to the recipes page after tapping the ElevatedButton.
+    expect(find.byType(MyRecipeFinder), findsOneWidget);
+  });
+}
